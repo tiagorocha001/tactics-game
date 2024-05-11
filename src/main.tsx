23b961f15ae3bc8704467e782d3298eaa5c64c1c
tiagorocha001 <ttavares@cpat.com>
2024-05-11 13:11:42 -0300

    Update system

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import { MantineProvider } from '@mantine/core';
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <MantineProvider>
+        <App />
+      </MantineProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
